8bee255062fe4cd8a992371a33a18bf9d1352fb6
James Garcia <jamesgarcia@Jamess-MacBook-Air.local>
2020-08-10 15:45:20 -0700

    updating html and css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-window.alert ("Presenting your game!");
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+//onmouse click add player chip to slot.
+
+/*----- functions -----*/
+function initialize() {
+
+    render()
+}
+
+function render() {
+
+}
