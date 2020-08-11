cacc66ada55b3769514e28d2606f2ad0f72e9d02
James Garcia <jamesgarcia@Jamess-MacBook-Air.local>
2020-08-10 19:47:50 -0700

    js working

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ for (let i = 0; i < 42; i++) {
     let square = document.createElement('div');
     let circle = document.createElement('div');
     circle.id = i;
-    circle.textContent = '';
+    //circle.textContent = '';
     square.appendChild(circle);
     document.getElementById('container').appendChild(square);
 }
@@ -33,7 +33,7 @@ let player
 
 
 /*----- event listeners -----*/
-//onmouse click add player chip to slot
+//onmouse click add player chip to slot and also makes the next players turn be set.
 document.getElementById('container').addEventListener('click', evt => {
     console.log(evt.target.id);
     board[evt.target.id] = player;
@@ -42,6 +42,8 @@ document.getElementById('container').addEventListener('click', evt => {
 })
 
 /*----- functions -----*/
+//this sets the board entire board to null so players can choose their slots
+//and makes player 1 go first and finally renders the game.
 function initialize() {
     board = new Array(42).fill(null);
     player = 1;
@@ -49,10 +51,17 @@ function initialize() {
 
     render()
 }
+function update() {
 
+}
+// based on the mouse click this function adds the current players choice to the board.
 function render() {
 board.forEach((element, id) => {
     document.getElementById(id).style.backgroundColor = LOOK_UP[element]
+   
 })
+
 }
+
+
 initialize()
