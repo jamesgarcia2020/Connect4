b0d1fe7c730c4f7ee624959878420ea1af2a0ecf
James Garcia <jamesgarcia@Jamess-MacBook-Air.local>
2020-08-13 15:16:52 -0700

    adding pics

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # Connect4
 
-<img src="https://i.imgur.com/4GxpHYt.png"/>
-
-Inline-style: 
-![alt text]("https://imgur.com/4GxpHYt"Logo Title Text 1")
+## Screen shot of main game page.
+<img src="https://i.imgur.com/4iJ9LlF.png"/>
 
 
 
+
+
+## Game play 
 This is a two player game.  Each player take a turn dropping a chip in a slot, player 1 goes first then player two moves.  The goal is to get 4 of your chips in a row before you oppoent does.  
 
+## The languages
 The technologies used are HTML, CSS, Javascript.
 
-Planned future enhancements include:
+## Planned future enhancements include:
 1. better graphics and CSS styling
 2. have score kept for each player as they win a round of the game.
 3. allow for customizable chips (example a chip with a cat as its image)
 4. have a musical loop running during game play and a special song for a winning move.
+
+## Link to the game
+[Connect 4](https://google.com)
 
 
--- js/main.js
+++ js/main.js
@@ -1,36 +1,26 @@
 /*----- constants -----*/
 for (let i = 0; i < 42; i++) {
-  let square = document.createElement('div');
-  let circle = document.createElement('div');
-  circle.id = i;
-  circle.textContent = 'hello';
-  square.appendChild(circle);
-  document.getElementById('container').appendChild(square);
-  console.log(circle.id)
-
+    let square = document.createElement('div');
+    let circle = document.createElement('div');
+    circle.id = i;
+    square.appendChild(circle);
+    document.getElementById('container').appendChild(square);
 }
-
 
 let reset = document.getElementById('reset');
 const msg = document.getElementById('msg');
 
-
-
-
-
-
-
 //This is showing each players chip color
 const LOOK_UP = {
-  1: 'red',
-  '-1': 'black',
-  null: 'white',
-};
+    1: 'red',
+    '-1': 'black',
+    null: 'white',
+}
 
 /*----- app's state (variables) -----*/
 //The state of the board 
 let board
-//The players state
+    //The players state
 let player
 let winner
 
@@ -48,79 +38,67 @@ document.getElementById('container').addEventListener('click', handleClick)
 //this sets the board entire board to null so players can choose their slots
 //and makes player 1 go first and finally renders the game.
 function initialize() {
-  board = new Array(42).fill(null);
-  player = 1;
-  winner = null;
-  render();
+    board = new Array(42).fill(null);
+    player = 1;
+    winner = null;
+    render();
 }
 
 function handleClick(evt) {
-  const index = parseInt(evt.target.id);
-  let column = index % 7 //column number 
-  console.log(column);
-  if (winner || board[column]) return; //if top of column has a value or game has a winner
-  let playerChoice = column
-  while (board[playerChoice + 7] === null && playerChoice + 7 < 42) {
-    //if the next space in the column is not empty and in the array go down to the next space
-    playerChoice += 7
-  };
-  board[playerChoice] = player;
-  console.log(horizontalCheck())
-  console.log(verticalCheck())
-  console.log(diagonalrl())
-  console.log(diagonallr())
-  winner = horizontalCheck() || verticalCheck() || diagonallr() || diagonalrl();
-  console.log(playerChoice)
-  player *= -1;
-  render()
+    const index = parseInt(evt.target.id);
+    let column = index % 7 //column number 
+
+    if (winner || board[column]) return; //if top of column has a value or game has a winner
+    let playerChoice = column
+    while (board[playerChoice + 7] === null && playerChoice + 7 < 42) {
+        //if the next space in the column is not empty and in the array go down to the next space
+        playerChoice += 7
+    };
+    board[playerChoice] = player;
+    winner = horizontalCheck() || verticalCheck() || diagonallr() || diagonalrl();
+    player *= -1;
+    render()
 }
+//win logic for all possible moves
 
 function diagonalrl() {
-  return board.some(function (element, idx) {
-    return idx % 6 > 2 && idx < 18 && Math.abs(board[idx] + board[idx + 6] + board[idx + 12] +
-      board[idx + 18]) === 4
-  })
+    return board.some(function(element, idx) {
+        return idx % 7 > 2 && idx < 21 && Math.abs(board[idx] + board[idx + 6] + board[idx + 12] +
+            board[idx + 18]) === 4
+    })
 }
 
 function diagonallr() {
-  return board.some(function (element, idx) {
-    return idx % 7 < 4 && Math.abs(board[idx] + board[idx + 8] + board[idx + 16] +
-      board[idx + 24]) === 4
-  })
+    return board.some(function(element, idx) {
+        return idx % 7 < 4 && Math.abs(board[idx] + board[idx + 8] + board[idx + 16] +
+            board[idx + 24]) === 4
+    })
 }
 
-
 function verticalCheck() {
-  return board.some(function (element, idx) {
-    return idx + 21 < board.length && Math.abs(board[idx] + board[idx + 7] + board[idx + 14] +
-      board[idx + 21]) === 4
-  })
+    return board.some(function(element, idx) {
+        return idx + 21 < board.length && Math.abs(board[idx] + board[idx + 7] + board[idx + 14] +
+            board[idx + 21]) === 4
+    })
 }
 
 function horizontalCheck() {
-  return board.some(function (element, idx) {
-    return idx % 7 < 4 && Math.abs(board[idx] + board[idx + 1] + board[idx + 2]
-      + board[idx + 3]) === 4
-  })
+    return board.some(function(element, idx) {
+        return idx % 7 < 4 && Math.abs(board[idx] + board[idx + 1] + board[idx + 2] +
+            board[idx + 3]) === 4
+    })
 }
-
 
 // based on the mouse click this function adds the current players choice to the board.
 function render() {
-  board.forEach((element, id) => {
-    document.getElementById(id).style.backgroundColor = LOOK_UP[element];
-    if (winner) {
-      msg.innerText = `Player ${player > 0 ? '1' : '2'} Wins!`
-    } else {
-      msg.innerText = `Player ${LOOK_UP[player].toUpperCase()}'s Turn`
-
-    }
-
-  });
-
+    board.forEach((element, id) => {
+        document.getElementById(id).style.backgroundColor = LOOK_UP[element];
+        if (winner) {
+            msg.innerText = `Player ${player > 0 ? '2' : '1'} Wins!`
+        } else {
+            msg.innerText = `Player ${LOOK_UP[player].toUpperCase()}'s Turn`
+        }
+    });
 }
-
-
-
 
 initialize()
